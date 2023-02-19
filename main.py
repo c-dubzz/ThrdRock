8e6b7c6bae75ae10d43dd3414e900f01c963fe16
Cole Walgren <colewalgren1@gmail.com>
2023-02-18 19:07:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,46 @@
 import requests
+import sqlite3
 from bs4 import BeautifulSoup
 
 
 def get_headlines(url):
-    # Make a GET request to the URL
+    # Extract the text from headlines on a given url
     response = requests.get(url)
-
-    # Parse the HTML content using Beautiful Soup
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Parse the HTML content using Beautiful Soup
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Find all the headline tags (h1, h2, h3, h4, h5, h6)
-    headlines = soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
-
-    # Extract the text content of each headline and store it in a list
+    content = BeautifulSoup(response.text, 'html.parser')
+    headlines = content.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
     headlines_text = [headline.get_text().strip()
                       for headline in headlines]
-
     return headlines_text
 
 
+# Connect to the database and create new headlines table
+conn = sqlite3.connect('headlines.db')
+c = conn.cursor()
+c.execute('CREATE TABLE IF NOT EXISTS headlines (title TEXT)')
+
+# Define the URL of the webpage to scrape and get headlines from it
 url = 'https://www.nationalgeographic.com/pages/topic/latest-stories'
 headlines = get_headlines(url)
-# print(headlines)
 
 url = 'https://www.nytimes.com/spotlight/learning-stem'
 headlines = get_headlines(url)
-# print(headlines)
 
 url = 'https://www.sciencedaily.com/news/'
 headlines = get_headlines(url)
-# print(headlines)
 
 url = 'https://www.theguardian.com/us/environment'
 headlines = get_headlines(url)
-# print(headlines)
 
 url = 'https://www.theguardian.com/us/technology'
 headlines = get_headlines(url)
-# print(headlines)
 
 url = 'https://www.theguardian.com/science'
 headlines = get_headlines(url)
-# print(headlines)
+
+# Insert each headline into database
+for headlines in headlines:
+    c.execute('''INSERT INTO headlines (headline) VALUES (?)''', (headlines,))
+
+# Commit changes and close connection
+conn.commit()
+conn.close()
